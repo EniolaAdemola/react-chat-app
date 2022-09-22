ddbf06efcc290f51e92c73c50c749f6743f5a487
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-22 23:24:32 +0100

    update button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Button = () => {
-	return <div>Button</div>;
+const Button = ({ bgColor, text, link }) => {
+	return (
+		<div>
+			<Link to={link} className="btn lg">
+				<button type="button" style={{ backgroundColor: bgColor }}>
+					{text}
+				</button>
+			</Link>
+		</div>
+	);
 };
 
 export default Button;
