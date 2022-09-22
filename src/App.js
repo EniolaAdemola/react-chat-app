13a4289d5dffe65281648fd2e31bf5440e450ad7
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-22 19:27:35 +0100

    add landing page route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landingPage/LandingPage";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
@@ -9,6 +10,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route index element={<LandingPage />} />
 				<Route path="login" element={<Login />} />
 				<Route path="register" element={<Register />} />
 				<Route path="home" element={<Home />} />
