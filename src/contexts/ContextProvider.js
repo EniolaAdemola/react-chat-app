f38e8f1bca337c77d54bccff2b45891e8b6814b9
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-22 23:18:37 +0100

    update context

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,16 +1,32 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
 export const ContextProvider = ({ children }) => {
+	const [signUp, setSignUp] = useState({
+		FName: "",
+		email: "",
+	});
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setSignUp((prevValue) => {
+			return {
+				...prevValue,
+				[name]: value,
+			};
+		});
+	};
+
 	return (
 		<StateContext.Provider
-			value={
-				{
-					// This is where i'm going to pass my
-					// all my global state hook
-				}
-			}
+			value={{
+				// This is where i'm going to pass my
+				// all my global state hook
+				signUp,
+				handleChange,
+			}}
 		>
 			{children}
 		</StateContext.Provider>
