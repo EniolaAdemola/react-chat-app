82f88d50240658b176df2fe2b8e39aff955f5898
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-27 19:41:08 +0100

    add log out function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import eniola from "../assets/images/eniola.png";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Navbar = () => {
 	return (
 		<div className="navbar">
 			<span className="logo">Eniola's Chat</span>
 			<div className="user">
-				<img src="" alt="" />
+				<img src={eniola} alt="" />
 				<span>David</span>
-				<button>Logout</button>
+				<button onClick={() => signOut(auth)}>Logout</button>
 			</div>
 		</div>
 	);
