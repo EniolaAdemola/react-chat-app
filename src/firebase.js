24c77d85395dbb38877202a2f1133863247708c5
Eniola Ademola <it.eniolaademola@gmail.com>
2022-09-28 16:29:28 +0100

    refactor firebase code

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyAEjfIf30gSc2ybzPIBJ-p586h3J0YEfaw",
 	authDomain: "eniola-chat-app.firebaseapp.com",
