405af1f93b61eb1eb01b69f475c69870f96c75d9
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-03 19:45:47 +0100

    add user profile image and name from firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
-import React from "react";
-import eniola from "../assets/images/eniola.png";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { StateContext } from "../contexts/ContextProvider";
 
 const Navbar = () => {
+	const { currentUser } = useContext(StateContext);
+
 	return (
 		<div className="navbar">
 			<span className="logo">Eniola's Chat</span>
 			<div className="user">
-				<img src={eniola} alt="" />
-				<span>David</span>
+				<img src={currentUser.photoURL} alt="" />
+				<span>{currentUser.displayName}</span>
 				<button onClick={() => signOut(auth)}>Logout</button>
 			</div>
 		</div>
