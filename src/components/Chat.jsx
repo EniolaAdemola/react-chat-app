0cfbc7b89dbdc8149b4f5cd012b24952aef9dd3f
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-05 21:20:20 +0100

    add user from firebase

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Add from "../assets/images/add.png";
 import More from "../assets/images/more.png";
 import Cam from "../assets/images/cam.png";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../contexts/ChatContext";
 
 const Chat = () => {
+	const { data } = useContext(ChatContext);
+
 	return (
 		<div className="chat">
 			<div className="chat__info">
-				<span>Daveworld</span>
+				<span>{data.user?.displayName}</span>
 				<div className="chat__icons">
 					<img src={Cam} alt="" />
 					<img src={Add} alt="" />
