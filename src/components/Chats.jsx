208fb6f6d342f6d0700a15c73e2937a9489771a5
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-05 21:21:24 +0100

    handle chats of a user

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect, useContext } from "react";
 import { doc, onSnapshot } from "firebase/firestore";
 import { db } from ".././firebase";
 import { StateContext } from "../contexts/ContextProvider";
+import { ChatContext } from "../contexts/ChatContext";
 
 const Chats = () => {
 	const [chats, setChats] = useState([]);
 
 	const { currentUser } = useContext(StateContext);
+	const { dispatch } = useContext(ChatContext);
 
 	useEffect(() => {
 		const getChats = () => {
@@ -21,11 +23,18 @@ const Chats = () => {
 		currentUser.uid && getChats();
 	}, [currentUser.uid]);
 
-	console.log(Object.entries(chats));
+	const handleSelect = (u) => {
+		dispatch({ type: "CHANGE_USER", payload: u });
+	};
+
 	return (
 		<div className="chats">
 			{Object.entries(chats)?.map((chat) => (
-				<div className="user__chat" key={chat[0]}>
+				<div
+					className="user__chat"
+					key={chat[0]}
+					onClick={() => handleSelect(chat[1].userInfo)}
+				>
 					<img src={chat[1].userInfo.photoURL} alt="userImage" />
 					<div className="user__chat-info">
 						<span>{chat[1].userInfo.displayName}</span>
