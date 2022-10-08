045a43eba052753c8b6f79ccbe1d87d00549ea37
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-08 12:40:52 +0100

    render all message from firebase

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -29,19 +29,22 @@ const Chats = () => {
 
 	return (
 		<div className="chats">
-			{Object.entries(chats)?.map((chat) => (
-				<div
-					className="user__chat"
-					key={chat[0]}
-					onClick={() => handleSelect(chat[1].userInfo)}
-				>
-					<img src={chat[1].userInfo.photoURL} alt="userImage" />
-					<div className="user__chat-info">
-						<span>{chat[1].userInfo.displayName}</span>
-						<p>{chat[1].userInfo.lastMessage?.text}</p>
+			{Object.entries(chats)
+				?.sort((a, b) => b[1].date - a[1].date)
+				.map((chat) => (
+					<div
+						className="user__chat"
+						key={chat[0]}
+						onClick={() => handleSelect(chat[1].userInfo)}
+					>
+						{console.log(chat)}
+						<img src={chat[1].userInfo?.photoURL} alt="userImage" />
+						<div className="user__chat-info">
+							<span>{chat[1].userInfo?.displayName}</span>
+							<p>{chat[1].lastMessage?.text}</p>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
 		</div>
 	);
 };
